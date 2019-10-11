6ad87295f710e08b00f20f93d0d8c7d5a5239051
benyafang <benyafang@neotel.com.cn>
2019-10-11 17:36:01 +0800

    clone

--- src/com/company/jian_zhi_offer/link_list/C23_Clone.java
+++ src/com/company/jian_zhi_offer/link_list/C23_Clone.java
@@ -10,8 +10,52 @@ import com.company.jian_zhi_offer.base_package.RandomListNode;
  */
 
 public class C23_Clone {
-    public RandomListNode Clone(RandomListNode pHead) {
-        return new RandomListNode(4);
+    public static RandomListNode Clone(RandomListNode pHead) {
+        if(pHead == null)return null;
+        if(pHead.next == null){
+            return pHead;
+        }
+        RandomListNode q = pHead;
+        while (q != null){
+//            System.out.println(q.label);
+            RandomListNode tmp = new RandomListNode(q.label);
+            tmp.next = q.next;
+            q.next = tmp;
+//            if(q.random != null) tmp.random = q.random.next;
+            q = q.next.next;
+        }
+        q = pHead;
+        while (q != null){
+            if(q.random != null)q.next.random = q.random.next;
+            q = q.next.next;
+        }
+        RandomListNode p = pHead.next;
+        q = pHead.next;
+        while (p != null && p.next != null){
+            p.next = p.next.next;
+            p = p.next;
+        }
+        return q;
+    }
+
+    public static void main(String[] args){
+        RandomListNode node1 = new RandomListNode(1);
+        RandomListNode node2 = new RandomListNode(2);
+        RandomListNode node3 = new RandomListNode(3);
+        RandomListNode node4 = new RandomListNode(4);
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node4;
+        node1.random = node4;
+        node3.random = node2;
+        RandomListNode res = Clone(node1);
+        System.out.println("---");
+        while (res != null){
+            System.out.println("=====");
+            System.out.println(res.label);
+            if(res.random != null)System.out.println(res.random.label);
+            res = res.next;
+        }
     }
 
 }
