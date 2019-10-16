75994c29416fb1865a5d6c0de9d4ffcb1d44bf7d
AlisaBen <yafang1826@163.com>
2019-10-17 00:41:56 +0800

    有效的括号

---
+++ src/com/company/leetcode/_15_threeSum.java
@@ -0,0 +1,55 @@
+package com.company.leetcode;
+
+import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+
+import java.util.*;
+
+/**
+ * created by benyafang on 2019/10/16 23:46
+ * List与ArrayList和new int[]的区别
+ * */
+public class _15_threeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        int i,j,k;
+        Set<List<Integer>> res = new HashSet<>();
+        for(i = 0;i < nums.length - 2;i++){
+            for(j = i + 1;j < nums.length - 1;j++){
+                for(k = j + 1;k < nums.length;k++){
+                    if(nums[i] + nums[j] + nums[k] == 0){
+                        List<Integer> tmp = new ArrayList<Integer>();
+                        tmp.add(nums[i]);
+                        tmp.add(nums[j]);
+                        tmp.add(nums[k]);
+                        Collections.sort(tmp, new Comparator<Integer>() {
+                            @Override
+                            public int compare(Integer o1, Integer o2) {
+                                return o1.compareTo(o2);
+                            }
+                        });
+                        res.add(tmp);
+                    }
+                }
+            }
+        }
+        return new ArrayList<>(res);
+
+    }
+    public static void main(String[] args){
+        Set<List<Integer>> a = new HashSet<>();
+        List<Integer> a1 = new ArrayList<>();
+        a1.add(3);
+        a1.add(4);
+        List<Integer> a2 = new ArrayList<>();
+        a2.add(3);
+        a2.add(4);
+        a.add(a1);
+        a.add(a2);
+//        a.add(4);
+//        a.add(4);
+        System.out.println(a);
+        List<List<Integer>> res = threeSum(new int[]{-1, 0, 1, 2, -1, -4});
+        for(int i = 0; i < res.size();i ++){
+            System.out.println(res.get(i));
+        }
+    }
+}
---
+++ src/com/company/leetcode/_20_isValid.java
@@ -0,0 +1,35 @@
+package com.company.leetcode;
+
+import java.util.Stack;
+
+/**
+ * created by benyafang on 2019/10/16 0:29
+ * */
+public class _20_isValid {
+    public static boolean isValid(String s) {
+        Stack<Character> tmp = new Stack<Character>();
+        for(int i = 0; i < s.length();i++){
+
+            if(s.charAt(i) == '{' || s.charAt(i) == '[' || s.charAt(i) == '('){
+                tmp.push(s.charAt(i));
+            }else if(s.charAt(i) == '}' || s.charAt(i) == ']' || s.charAt(i) == ')'){
+
+                /**
+                 * pop出栈的时候一定要判断栈是否为空
+                 * */
+                if(tmp.empty())return false;
+                char t = tmp.pop();
+                if(!(t == '{' && s.charAt(i) == '}' || t == '[' && s.charAt(i) == ']' || t == '(' && s.charAt(i) == ')'))
+                    return false;
+            }
+        }
+
+        return tmp.empty();
+
+    }
+    public static void main(String[] args){
+        Boolean res = isValid("[");
+        System.out.println(res);
+    }
+}
+
