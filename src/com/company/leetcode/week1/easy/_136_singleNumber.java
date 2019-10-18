3d4f6911c77727d439cad1ebf37a6ad18521e4f6
AlisaBen <yafang1826@163.com>
2019-10-18 23:24:26 +0800

    只出现一次的数字

--- src/com/company/leetcode/week1/easy/_136_singleNumber.java
+++ src/com/company/leetcode/week1/easy/_136_singleNumber.java
@@ -7,6 +7,18 @@ import java.util.Stack;
 /**
  * created by benyafang on 2019/10/17 9:10
  * 内存消耗
+ * 位运算，异或
  * */
 public class _136_singleNumber {
+    public static int singleNumber(int[] nums) {
+        int v = 0 ;
+        for(int x:nums){
+            v ^= x;
+        }
+        return v;
+
+    }
+    public static void main(String[] args){
+        System.out.println(singleNumber(new int[]{2}));
+    }
 }
