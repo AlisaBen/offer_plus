c2fd7e3e7d49fd596947f0ad15a550cd96c30b8f
AlisaBen <yafang1826@163.com>
2019-10-18 23:39:25 +0800

    两数之和

--- src/com/company/leetcode/week1/easy/_1_twoSum.java
+++ src/com/company/leetcode/week1/easy/_1_twoSum.java
@@ -1,20 +1,33 @@
 package com.company.leetcode.week1.easy;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * created by benyafang on 2019/10/15 22:30
  * */
 public class _1_twoSum {
     public static int[] twoSum(int[] nums, int target) {
         for(int i = 0; i < nums.length - 1; i++){
-//            if(nums[i] <= target){
                 for(int j = i+1; j < nums.length;j++){
-//                    System.out.println(j);
                     if(nums[i] + nums[j] == target)return new int[]{i,j};
                 }
-//            }
         }
-        return new int[]{};
+        throw new IllegalArgumentException("no sum");
     }
+    public static int[] twoSum1(int [] nums,int target){
+        Map<Integer,Integer> tmp = new HashMap<>();
+        for(int i = 0; i < nums.length;i++){
+            tmp.put(nums[i],i);
+        }
+        for(int i = 0;i < nums.length;i++){
+            if(tmp.containsKey(target - nums[i]) && tmp.get(target - nums[i]) != i){
+                return new int[]{i,tmp.get(target - nums[i])};
+            }
+        }
+        throw new IllegalArgumentException("");
+    }
+
 
     public static void main(String[] args){
         int[] res = twoSum(new int[]{-1,-2,-3,-4,-5},-8);
