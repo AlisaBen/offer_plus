582af363af116c91c3d7c30faeb89522b30b88cb
AlisaBen <yafang1826@163.com>
2019-10-25 08:14:38 +0800

    盛最多水的容器

--- src/com/company/leetcode/week2/middle/_11_maxArea.java
+++ src/com/company/leetcode/week2/middle/_11_maxArea.java
@@ -1,8 +1,20 @@
 package com.company.leetcode.week2.middle;
 
 public class _11_maxArea {
-    public int maxArea(int[] height) {
-        return 0;
-
+    public static int maxArea(int[] height) {
+        int result = 0;
+        for(int i = 0;i < height.length - 1;i ++){
+            int tmp = 0;
+            for(int j = i+1;j < height.length;j++){
+                tmp = Math.max(tmp,Math.min(height[i],height[j]) * (j - i));
+            }
+            result = Math.max(result,tmp);
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        int[] height = {1,8,6,2,5,4,8,3,7};
+        int result = maxArea(height);
+        System.out.println(result);
     }
 }
