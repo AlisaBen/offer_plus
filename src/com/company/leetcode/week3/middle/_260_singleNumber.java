04f5c7dbc4827e19e3c3b00d1904479daab43f57
AlisaBen <yafang1826@163.com>
2019-10-29 22:33:42 +0800

    只出现一次的数字II

--- src/com/company/leetcode/week3/middle/_260_singleNumber.java
+++ src/com/company/leetcode/week3/middle/_260_singleNumber.java
@@ -1,7 +1,30 @@
 package com.company.leetcode.week3.middle;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class _260_singleNumber {
-    public int[] singleNumber(int[] nums) {
-        return null;
+    public static int[] singleNumber(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0;i < nums.length;i++){
+            if(map.containsKey(nums[i]))map.put(nums[i],map.get(nums[i]) + 1);
+            else map.put(nums[i],1);
+        }
+        ArrayList<Integer> tmp = new ArrayList<>();
+        for(Integer t :map.keySet()){
+            if(map.get(t) == 1)tmp.add(t);
+        }
+        int[] result = new int[tmp.size()];
+        for(int i = 0;i < tmp.size();i++){
+            result[i] = tmp.get(i);
+        }
+        return result;
+    }
+    public static void main(String[] args){
+        int [] nums= {1,2,3,4,2,3};
+        int[] result = singleNumber(nums);
+        System.out.println(Arrays.toString(result));
     }
 }
