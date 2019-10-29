49c6836fdfec66a0ddabd6be48b03acf983145f7
AlisaBen <yafang1826@163.com>
2019-10-29 20:51:00 +0800

    验证回文串

--- src/com/company/leetcode/week3/easy/_125_isPalindrome.java
+++ src/com/company/leetcode/week3/easy/_125_isPalindrome.java
@@ -1,8 +1,48 @@
 package com.company.leetcode.week3.easy;
 
 public class _125_isPalindrome {
-    public boolean isPalindrome(String s) {
+    public static boolean isValid(char c){
+        return c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || Character.isDigit(c);
+    }
+    public static boolean isPalindrome(String s) {
+        if(s.length() <= 1)return true;
+        int i = 0;
+        int j = s.length() - 1;
+        while (i < j){
+            char c1 = s.charAt(i);
+            char c2 = s.charAt(j);
+            if(!isValid(c1)){
+                i++;
+                continue;
+            }
+            if(!isValid(c2)){
+                j--;
+                continue;
+            }
+            if(Character.isDigit(c1) && Character.isDigit(c2)){
+                if(c1 == c2){
+                    i++;
+                    j--;
+                }else return false;
+            }else if(!Character.isDigit(c1) && !Character.isDigit(c2)){
+                if(Character.toLowerCase(c1) == Character.toLowerCase(c2)){
+                    i++;j--;
+                }else return false;
+            }else return false;
+
+        }
         return true;
 
     }
+    public static void main(String[] args){
+//        System.out.println(isValid('c'));
+//        System.out.println(isValid('A'));
+//        System.out.println(isValid('2'));
+//        System.out.println(isValid(';'));
+//        System.out.println(Character.toLowerCase('A'));
+        boolean result = isPalindrome("race a car");
+        System.out.println(result);
+
+    }
+
 }
