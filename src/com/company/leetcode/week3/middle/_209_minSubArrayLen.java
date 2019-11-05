660d81dc7fadafb82a275b0220b4b4b986a49e3c
AlisaBen <yafang1826@163.com>
2019-11-05 22:17:31 +0800

    最小数组和

--- src/com/company/leetcode/week3/middle/_209_minSubArrayLen.java
+++ src/com/company/leetcode/week3/middle/_209_minSubArrayLen.java
@@ -1,33 +1,34 @@
 package com.company.leetcode.week3.middle;
 
+/**
+ * created by Arya on 2019/11/5 22:17
+ * 滑动窗口
+ * */
 public class _209_minSubArrayLen {
-    public int minSubArrayLen(int s, int[] nums) {
-        int result = Integer.MAX_VALUE;
+    public static int minSubArrayLen(int s, int[] nums) {
         int sum = 0;
+        int first = 0;//滑动窗口左侧索引
+        int last = 0;//滑动窗口右侧索引
+        int result = nums.length;//默认大于s的长度为数组长度
         boolean flag = false;
-        for(int i = 0;i < nums.length;i++){
-            if(sum < s){
-                sum += nums[i];
-                if(sum >= s){
-                    result = i + 1;
-                    flag = true;
-                }
-            }else{
-                if (sum - nums[i - result] + nums[i] >= s){
-                    int j = i - result + 1;
-                    sum = sum - nums[i - result] + nums[i];
-                    while (j <= i && sum - nums[j] > s){
-                        sum = sum - nums[j];
-                        result--;
-                    }
-                }
-
-
-
+        while (last < nums.length){
+            //循环的条件是滑动窗口的右侧索引小于数组长度
+            if(sum + nums[last] < s){
+                sum += nums[last++];//sum加上当前的数值，然后右侧索引++
+            }else{//sum+nums[last] >= s
+                flag = true;
+                result = Math.min(result,last - first + 1);
+                sum -= nums[first++];//因为下一次循环比较的时候仍然要比较sum+nums[last]的值所以这里的sum不进行加法
+                //判断sum减去滑动窗口左侧的数值是否仍然满足条件
+//                result = Math.min(result,last - first + 1);
             }
 
-
         }
-        return 0;
+
+        return flag ? result : 0;
+    }
+    public static void main(String[] args){
+        int res = minSubArrayLen(7,new int[]{2,3,1,2,4,3});
+        System.out.println(res);
     }
 }
--- src/com/company/leetcode/week3/middle/_77_combine.java
+++ src/com/company/leetcode/week3/middle/_77_combine.java
@@ -1,9 +1,47 @@
 package com.company.leetcode.week3.middle;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * 回溯法
+ * */
 public class _77_combine {
+    List<List<Integer>> output = new LinkedList<>();
+    int n;
+    int k;
+    public void backtrack(int first, LinkedList<Integer> curr){
+        if(curr.size() == k)
+            output.add(new LinkedList<>(curr));
+        for(int i = first;i < n + 1; ++i){
+            curr.add(i);
+            backtrack(i + 1, curr);
+            curr.removeLast();
+        }
+    }
     public List<List<Integer>> combine(int n, int k) {
-        return null;
+        this.n = n;
+        this.k = k;
+        backtrack(1,new LinkedList<>());
+        return output;
+    }
+    public List<List<Integer>> combine1(int n,int k){
+        LinkedList<Integer> nums = new LinkedList<>();
+        for(int i = 1;i < k + 1; ++i){
+            nums.add(i);
+        }
+        nums.add(n + 1);
+        List<List<Integer>> output = new ArrayList<List<Integer>>();
+        int j = 0;
+        while (j < k){
+            output.add(new LinkedList<>(nums.subList(0,k)));
+            j = 0;
+            while ((j < k) && (nums.get(j + 1) == nums.get(j) + 1))
+                nums.set(j,j ++ + 1);
+            nums.set(j,nums.get(j) + 1);
+        }
+        return output;
+
     }
 }
